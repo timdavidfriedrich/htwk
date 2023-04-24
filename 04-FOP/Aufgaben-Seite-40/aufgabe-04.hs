a441e3921b930a58bcc4366109670c68b79c059e
Tim <timdavidfriedrich@gmail.com>
2023-04-24 11:20:05 +0200

    aufgabe 4 fertig, I guess

--- 04-FOP/Aufgaben-Seite-40/aufgabe-04.hs
+++ 04-FOP/Aufgaben-Seite-40/aufgabe-04.hs
@@ -1,26 +1,68 @@
 import qualified Prelude
--- import Test.LeanCheck
+import Test.LeanCheck
+import GHC.Generics
+import Test.LeanCheck.Generic
+import Test.LeanCheck.Utils
 
-data N = Z | S N deriving Prelude.Show
 
+-- Spezifikation für min und max:
+-- min (x + y) y == y && min x (x + y) == x
+-- max x (x + y) == x + y && max (x + y) y == x + y
+
+
+-- Datentyp für Peano-Zahlen
+data N = Z | S N deriving (Prelude.Eq, Generic, Prelude.Show)
+
+-- Listable für N, damit LeanCheck mit N funktioniert 
+instance Listable N where tiers :: [[N]]
+                          tiers = genericTiers;
+
+-- Implementierung Addition
 plus :: N -> N -> N
 plus Z y = y ; plus (S x) y = S (plus x y)
 
+-- Implementierung Subtraktion
 minus :: N -> N -> N
 minus x Z = x ; minus (S x) (S y) = minus x y; minus Z x = Z
 
+-- Implementierung Maximum
 max :: N -> N -> N
 max x y = plus (minus x y) y
 
+-- LeanCheck-Test für Maximum
+max_check :: N -> N -> Prelude.Bool
+max_check = \ x y -> max (plus x y) y Prelude.== plus x y Prelude.&& max x (plus x y) Prelude.== plus x y;
+
+-- Implementierung Minimum
 min :: N -> N -> N
 min x y = minus (max x y) (max (minus x y) (minus y x))
 
+-- LeanCheck-Test für Minimum
+min_check :: N -> N -> Prelude.Bool
+min_check = \ x y -> min (plus x y) y Prelude.== y Prelude.&& min x (plus x y) Prelude.== x;
+
+-- Implementierung Median für 3 Argumente
 median :: N -> N -> N -> N
 median x y z = minus (minus (plus x (plus y z)) (max (max x y) z)) (min (min x y) z)
 
+-- LeanCheck-Test für Median
+median_check :: N -> N -> N -> Prelude.Bool
+median_check = \ x y z -> median x y z Prelude.== median y x z Prelude.&& median x y z Prelude.== median x z y Prelude.&& median x y z Prelude.== median y z x Prelude.&& median x y z Prelude.== median z x y Prelude.&& median x y z Prelude.== median z y x Prelude.&& median x y z Prelude.== median (median x y z) (median x y z) (median x y z);
 
--- forall x :: N, y :: N : min (plus x y) y = y
--- forall x :: N, y :: N : min x (plus x y) = x
 
--- forall x :: N, y :: N : max (plus x y) y = plus x y
--- forall x :: N, y :: N : max x (plus x y) = plus x y 
+
+-- Test-Fälle, falls manuelles Testen notwendig ist
+median_test_cases :: [(N, N, N, N)]
+median_test_cases = 
+            [ (S Z, S (S Z), S (S (S Z)), S (S Z))
+            , (S (S (S Z)), S (S Z), S Z, S (S Z))
+            , (S (S Z), S (S (S Z)), S Z, S (S Z))
+            , (S (S (S Z)), S Z, S (S Z), S (S Z))
+            , (S (S Z), S Z, S (S (S Z)), S (S Z))
+            , (S Z, S (S (S Z)), S (S Z), S (S Z))
+            , (S (S Z), S (S Z), S (S Z), S (S Z))
+            , (S Z, S Z, S (S Z), S Z)
+            , (S (S Z), S Z, S Z, S Z)
+            , (S(Z), (S(S Z)), S Z, S Z)
+            , (Z, Z, Z, Z)
+            ];
