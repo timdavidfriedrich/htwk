ee29a79f9963335612e6881d1021f906ef9ae81b
Tim <76115288+timdavidfriedrich@users.noreply.github.com>
2023-06-25 19:30:31 +0200

    Add files via upload

---
+++ 04-FOP/KW26-Lazy_Evaluation/aufgabe-01.hs
@@ -0,0 +1,18 @@
+-- y ist strikt
+f x y z = case y || x of
+    False -> y
+    True -> case z && y of
+        False -> z
+        True -> False
+
+g x y z = case x || y of
+    False -> y
+    True -> case z || y of
+        False -> z
+        True -> False
+
+h x y z = case x < y of
+    False -> x + y
+    True -> case x + y == y || z > x of
+        False -> x + z
+        True -> y
---
+++ 04-FOP/KW26-Lazy_Evaluation/aufgabe-01_loesungen.hs
@@ -0,0 +1,20 @@
+-- y ist strikt
+f x y z = case y || x of
+    False -> y
+    True -> case z && y of
+        False -> z
+        True -> False
+
+-- x ist strikt
+g x y z = case x || y of
+    False -> y
+    True -> case z || y of
+        False -> z
+        True -> False
+
+-- x und y sind strikt
+h x y z = case x < y of
+    False -> x + y
+    True -> case x + y == y || z > x of
+        False -> x + z
+        True -> y
