54946275be2cb88031adc500a378285ef79de170
Tim <76115288+timdavidfriedrich@users.noreply.github.com>
2023-06-25 19:38:52 +0200

    Update aufgabe-01.hs

--- 04-FOP/KW26-Lazy_Evaluation/aufgabe-01.hs
+++ 04-FOP/KW26-Lazy_Evaluation/aufgabe-01.hs
@@ -12,7 +12,7 @@ g x y z = case x || y of
         True -> False
 
 h x y z = case x < y of
-    False -> x + y
+    False -> x + y + z
     True -> case x + y == y || z > x of
         False -> x + z
         True -> y
